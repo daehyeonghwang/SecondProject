51bc7c21e496e0388c553d6b24186220fcf5a328
daehyeonghwang <svbd42@naver.com>
2015-12-28 09:53:11 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,11 @@
+ //for command line git tutorial
+ //GitHub Modification
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("What is Git?");
 		System.out.println("That's DVCS");
 		System.out.println("2002 Bitkeeper");
+		
 	}
 }
